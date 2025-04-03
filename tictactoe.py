823263e3b138f52f3edcd1926bfbba56f2ba2c4a
Uzair <uzairuzair050@gmail.com>
2025-04-03 21:33:04 +0500

    Added Player_2 functionality

--- tictactoe.py
+++ tictactoe.py
@@ -10,3 +10,5 @@ p2 = StringVar()
 
 player1_name = Entry(tk, textvariable = p1, bd = 5)
 player1_name = Entry(row = 1, column = 1, columnspan = 8)
+player2_name = Entry(tk, textvariable = p2, bd = 5)
+player2_name = Entry(row = 2, column = 1, columnspan = 8)
