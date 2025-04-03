dd37c7d60d6b096f87f36e7c0c56eba6c04e8676
Uzair <uzairuzair050@gmail.com>
2025-04-03 21:36:17 +0500

    Added Disable Button functionality

--- tictactoe.py
+++ tictactoe.py
@@ -12,3 +12,17 @@ player1_name = Entry(tk, textvariable = p1, bd = 5)
 player1_name = Entry(row = 1, column = 1, columnspan = 8)
 player2_name = Entry(tk, textvariable = p2, bd = 5)
 player2_name = Entry(row = 2, column = 1, columnspan = 8)
+
+
+bclick = True
+flag = 0
+def disableButton():
+    button1.configure(state = DISABLED)
+    button2.configure(state = DISABLED)
+    button3.configure(state = DISABLED)
+    button4.configure(state = DISABLED)
+    button5.configure(state = DISABLED)
+    button6.configure(state = DISABLED)
+    button7.configure(state = DISABLED)
+    button8.configure(state = DISABLED)
+    button9.configure(state = DISABLED)
