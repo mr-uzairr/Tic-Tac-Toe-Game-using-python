3aad7f0d9ca805a955df91c34cf125b53120665c
Uzair <uzairuzair050@gmail.com>
2025-04-03 21:31:52 +0500

    Added Player_1 functionality

--- tictactoe.py
+++ tictactoe.py
@@ -3,3 +3,10 @@ import tkinter.messagebox
 tk = Tk()
 tk.title("Tic Tac Toe Game")
 
+PlayerA = StringVar()
+PlayerB = StringVar()
+p1 = StringVar()
+p2 = StringVar()
+
+player1_name = Entry(tk, textvariable = p1, bd = 5)
+player1_name = Entry(row = 1, column = 1, columnspan = 8)
