d5e2aaf446559903e8a80ceaf691e5b58dbbb2da
Uzair <uzairuzair050@gmail.com>
2025-04-13 21:38:49 +0500

    Ask move from playerB

--- tictactoe.py
+++ tictactoe.py
@@ -72,7 +72,8 @@ def checkforwin():
         button2['text'] == "O" and button5['text'] == "O" and button8['text'] == "O" or
         button7['text'] == "O" and button6['text'] == "O" and button9['text'] == "O"):
         disableButton()
-        
+        tkinter.messagebox.showinfo("Tic Tac Toe", playerB)
+
 
 
 
