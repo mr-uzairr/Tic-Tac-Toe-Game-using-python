29ba4e6f2e825d27c741025bd5a92e50ae4cc6d0
Uzair <uzairuzair050@gmail.com>
2025-04-13 21:37:20 +0500

    disable button after player Selects "O"

--- tictactoe.py
+++ tictactoe.py
@@ -71,6 +71,8 @@ def checkforwin():
         button1['text'] == "O" and button2['text'] == "O" and button3['text'] == "O" or
         button2['text'] == "O" and button5['text'] == "O" and button8['text'] == "O" or
         button7['text'] == "O" and button6['text'] == "O" and button9['text'] == "O"):
+        disableButton()
+        
 
 
 
