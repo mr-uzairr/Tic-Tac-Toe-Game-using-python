deb37b26f6cbcb470c9218592c817c181bd63910
Uzair <uzairuzair050@gmail.com>
2025-04-13 21:25:59 +0500

    Updated checkforwin() function

--- tictactoe.py
+++ tictactoe.py
@@ -47,15 +47,15 @@ def btnClick(buttons):
         tkinter.messagebox.showinfo("Tic Tac Toe","Button already clicked")    
 
 def checkforwin():
-    if(button1['text'] == "X" and button2['text'] == "X" and button3['text'] == "X") or
-    if(button1['text'] == "X" and button2['text' == "X" and button3['text'] == "X") or
-    if(button1['text'] == "X" and button2['text' == "X" and button3['text'] == "X") or
-    if(button1['text'] == "X" and button2['text' == "X" and button3['text'] == "X") or
-    if(button1['text'] == "X" and button2['text' == "X" and button3['text'] == "X") or
-    if(button1['text'] == "X" and button2['text' == "X" and button3['text'] == "X") or
-    if(button1['text'] == "X" and button2['text' == "X" and button3['text'] == "X") or
-    if(button1['text'] == "X" and button2['text' == "X" and button3['text'] == "X") or
-    if(button1['text'] == "X" and button2['text' == "X" and button3['text']] == "X") or
+    if(button1['text'] == "X" and button2['text'] == "X" and button3['text'] == "X" or
+    button4['text'] == "X" and button5['text'] == "X" and button6['text'] == "X" or
+    button7['text'] == "X" and button8['text'] == "X" and button9['text'] == "X" or
+    button1['text'] == "X" and button5['text'] == "X" and button9['text'] == "X" or
+    button3['text'] == "X" and button5['text'] == "X" and button7['text'] == "X" or
+    button1['text'] == "X" and button2['text'] == "X" and button3['text'] == "X" or
+    button1['text'] == "X" and button2['text'] == "X" and button3['text'] == "X" or
+    button2['text'] == "X" and button5['text'] == "X" and button8['text'] == "X" or
+    button7['text'] == "X" and button6['text'] == "X" and button9['text'] == "X"):
 
     
     
